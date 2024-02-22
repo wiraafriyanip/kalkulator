893b357416aad504cbdf944d8dfbc038f801bacf
wiraafriyanip <wiraafriyanip@gmail.com>
2024-02-22 14:37:01 +0700

    satu

---
+++ app/Http/Controllers/ItungController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ItungController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $a=$request->a;
+        $b=$request->b;
+        $op=$request->op;
+         
+        if($op=='+'){
+            $hasil = $a+$b;
+        }elseif($op=='-'){
+            $hasil = $a-$b;
+        }elseif($op=='x'){
+            $hasil = $a*$b;
+        }elseif($op=='/'){
+            $hasil = $a/$b;
+        }elseif ($op == 'Reset') {
+            
+            $a = 0; 
+            $b = 0; 
+            $hasil = 0;
+        }
+
+        return view ('kalkulator.hasil',compact('hasil'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/kalkulator/index.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>box</title>
+    <style> 
+   body {
+  background: linear-gradient( #E2BFB3, #F7DED0, #FEECE2, #FFBE98);
+  /* Ganti warna sesuai keinginan Anda dan sesuaikan arah gradasi */
+  
+  font-family: 'Arial', sans-serif; /* Ganti jenis font sesuai keinginan Anda */
+  padding: 20px; /* Menambahkan padding agar konten tidak berdekatan dengan tepi */
+  color: #333; /* Warna teks pada body */
+}
+
+    
+/* CSS untuk styling tabel */
+table {
+  width: 40%;
+  hight: 40%;
+  border-collapse: collapse;
+  margin-bottom: 20px;
+  font-size: 16px; /* Ubah ukuran font sesuai kebutuhan */
+  background-color: #FFBE98;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+}
+
+th, td {
+  padding: 12px;
+  text-align: left;
+  border-bottom: 2px solid #ddd; /* Garis pembatas antar baris */
+  color: #000000;
+  font-family: Georgia, serif;
+}
+
+/* CSS untuk styling header kolom */
+th {
+  background-color: #f2f2f2; /* Warna latar belakang header */
+  font-weight: bold;
+  color: #000000; /* Warna teks header */
+}
+
+/* CSS untuk hover pada baris tabel */
+/* tr:hover {
+  background-color: #f5f5f5; /* Warna latar belakang saat hover */
+ */
+h1 {
+  color: #000000;
+  margin-bottom: 20px;
+  font-family: Georgia, serif; /* Added a fallback font */
+}
+
+
+
+
+ </style>  
+    
+</head>
+<body>
+    <h1><center>KALKULATOR W</center></h1>
+    <center>
+    
+    <br>
+    <form action=" {{ route('itung.store') }} " method="post">
+        @csrf
+    <table border=1 width=500>
+        <tr>
+            <td colspan=2><input type="number" name="a" id=""></td>
+            
+            <td colspan=2 rowspan=2><input type="submit" value="reset" name="op"></td>
+            
+        </tr>
+        <tr>
+            <td colspan=2><input type="number" name="b" id=""></td>
+            
+        </tr>
+        <tr>
+            <td colspan=4>Hasil</td>
+            
+        </tr>
+        <tr>
+            <td><input type="submit" value="+" name="op"></td>
+            <td><input type="submit" value="-" name="op"></td>
+            <td><input type="submit" value="x" name="op"></td>
+            <td><input type="submit" value="/" name="op"></td>
+            <td><input type="submit" value="Reset" name="op"></td>
+        </tr>
+        <tr>
+            <td colspan=4>
+                Nama :Wira Afriyani Putri
+                <br>
+                Kelas:XII PPLG 2
+            </td>
+            
+        </tr>
+</center>   
+</body>
+
+</html>
